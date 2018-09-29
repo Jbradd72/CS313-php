2e6f0310494acdbcf68cadc84eb31afd52d3eb67
Jbradd72 <bra16024@byui.edu>
2018-09-28 20:57:22 -0600

    Final Push for week 2

--- web/index.js
+++ web/index.js
@@ -1,9 +1,17 @@
-$(document).ready(function(){
-    $("#bio h3").hover(function(){
-        $("p b").show();
+$(document).ready(function () {
+    $("#bio h3").click(function () {
+        if ($("p b").css("display") == "none") {
+            $("p b").show();
+        } else {
+            $("p b").hide();
+        }
     });
-    
-     $("#quote h3").hover(function(){
-        $("blockquote").show();
+
+    $("#quote h3").click(function () {
+            if ($("blockquote").css("display") == "none") {
+            $("blockquote").show();
+        } else {
+            $("blockquote").hide();
+        }
     });
 });
