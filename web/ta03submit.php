b378dcb5cc33ec4d91c58d2ceb95c8a09b62a6e7
Jbradd72 <bra16024@byui.edu>
2018-10-03 12:31:51 -0600

    Starting ta03

--- web/ta03submit.php
+++ web/ta03submit.php
@@ -9,10 +9,19 @@
 
 <body>
     <h1>Your Info</h1>
-    <p>  Name: <?php echo$_POST["name"]; ?> <br>
-         Email: <?php echo$_POST["email"]; ?> <br>
-    Major: <?php echo$_POST["Major"]; ?> <br>
-    Comments: <?php echo$_POST["comments"]; ?> <br></p>
+    <p>  Name: <?php echo $_POST["name"]; ?> <br>
+         Email: <?php echo $_POST["email"]; ?> <br>
+    Major: <?php echo $_POST["Major"]; ?> <br>
+    Comments: <?php echo $_POST["comments"]; ?> <br>
+    You have visited: <br>
+    <?php 
+        $continents = $_POST["continents"];
+        $count = count($continents);
+        
+        for ($i=0; $i < $count; $i++){
+            echo "$continents[$i] <br>";
+        }
+        ?></p>
 
 
 </body>
