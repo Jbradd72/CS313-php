4dc9ddda2484286fd52119466ddf070f39ce35fa
Jbradd72 <bra16024@byui.edu>
2018-10-03 12:12:26 -0600

    Starting ta03

--- web/ta03submit.php
+++ web/ta03submit.php
@@ -1,18 +1,4 @@
 <?php $name = $_POST["name"]; ?>
-<html>
 
-<head>
-    <title>Teach Activity 3</title>
-    
-
-</head>
-
-<body>
-    <h1>Your Info</h1>
-    <p> <?php echo Name: $_POST["name"]; ?> <br> </p>
-
-
-</body>
-</html>
 
 
