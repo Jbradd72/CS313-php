968f0cd7cc5b03511bbcebf189f8e4a35cdf4abc
Jbradd72 <bra16024@byui.edu>
2018-10-03 19:13:49 -0600

    Finishing ta03

--- web/ta03submit.php
+++ web/ta03submit.php
@@ -3,18 +3,18 @@
 
 <head>
     <title>Teach Activity 3</title>
-    
+
 
 </head>
 
 <body>
     <h1>Your Info</h1>
-    <p>  Name: <?php echo $_POST["name"]; ?> <br>
-         Email: <?php echo $_POST["email"]; ?> <br>
-    Major: <?php echo $_POST["Major"]; ?> <br>
-    Comments: <?php echo $_POST["comments"]; ?> <br>
-    You have visited: <br>
-    <?php 
+    <p> Name:
+        <?php echo $_POST["name"]; ?> <br> Email: <a href="mailto:" <?php echo $_POST[ "email"]; ?>>
+        <?php echo $_POST["email"]; ?></a> <br> Major:
+        <?php echo $_POST["Major"]; ?> <br> Comments:
+        <?php echo $_POST["comments"]; ?> <br> You have visited: <br>
+        <?php 
         $continents = $_POST["continents"];
         $count = count($continents);
         $contMap = ["na" => "North America",
@@ -33,6 +33,5 @@
 
 
 </body>
+
 </html>
-
-
