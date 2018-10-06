f07c6be507452c7b101c479550ea7bcd02f0eb39
Jbradd72 <bra16024@byui.edu>
2018-10-05 20:20:18 -0600

    Testing week3

--- web/week3-footer.php
+++ web/week3-footer.php
@@ -1,5 +1,5 @@
 <!-- Footer -->
-    <footer class="py-5 bg-dark navbar-fixed-bottom">
+    <footer class="py-5 bg-dark navbar navbar-fixed-bottom">
       <div class="container">
         <p class="m-0 text-center text-white ">Copyright &copy; Your Website 2017</p>
       </div>
