1b43eace74a9e54abbea64fee9ce1cf0760c907a
Jbradd72 <bra16024@byui.edu>
2018-10-20 13:34:13 -0600

    Testing login

--- web/DigitalScrapBooking/mypage.php
+++ web/DigitalScrapBooking/mypage.php
@@ -1,8 +1,9 @@
 <?php  
+
+    require ('dbconfig.php');
     if (!isset($_SESSION['username'])){
         header('location: login.php');
     }
-    require ('dbconfig.php');
     require ('header.php');
     echo $_SESSION['username'];
     require ('footer.php');
