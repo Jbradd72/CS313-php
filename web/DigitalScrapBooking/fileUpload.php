1a82cc296b443626e4515659f64da829687ccd33
Jbradd72 <bra16024@byui.edu>
2018-10-26 22:21:55 -0600

    meh

--- web/DigitalScrapBooking/fileUpload.php
+++ web/DigitalScrapBooking/fileUpload.php
@@ -8,6 +8,33 @@
 </head>
 
 <body style ="text-align: center;">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="test1.html">
+            <h4>DSB</h4>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="mypage.php">My Page <span class="sr-only">(current)</span></a>
+                </li>
+                <li>
+                   <?php if (isset($_SESSION['username'])) 
+                    echo "<a class='nav-link' href='mypage.php'>My Page <span class='sr-only'></span></a>";
+                    else
+                    echo "<a class='nav-link' href='login.php'>Login <span class='sr-only'></span></a>"; ?>
+                </li>
+
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
     <progress style="margin-top: 15%;" value="0" max="100" id="uploader">0%</progress><br>
     <input style="margin-top: 5%;" type="file" value="upload" id="fileButton" />
     <br>
