39e91b39a8cf5944795584cdf20c3363b5d660b3
Jbradd72 <bra16024@byui.edu>
2018-11-03 19:17:49 -0600

    Fixing all sorts of ui stuff

--- web/DigitalScrapBooking/loadFiles.php
+++ web/DigitalScrapBooking/loadFiles.php
@@ -26,7 +26,7 @@ $video = $db->query("SELECT accounts.username, video.filename AS filename FROM v
         }
 }
 
-if ($type == 'photo'){
+if ($type == "photo"){
 $photo = $db->query("SELECT accounts.username, photo.filename AS filename FROM photo JOIN accounts ON accounts.accountid = photo.accountid AND accounts.username = '".$_SESSION['username']."'");
         
         
